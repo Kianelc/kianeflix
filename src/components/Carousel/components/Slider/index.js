3066a32a0afd1ae8c96222e81800c16e1d8950cb
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2020-07-30 00:28:49 -0300

    arrumando o slider para ser infinito

--- src/components/Carousel/components/Slider/index.js
+++ src/components/Carousel/components/Slider/index.js
@@ -42,8 +42,9 @@ const Slider = ({ children }) => (
     <SlickSlider
       {...{
         dots: false,
-        infinite: false,
+        infinite: true,
         speed: 300,
+        slidesToShow: 3,
         centerMode: false,
         variableWidth: true,
         adaptiveHeight: true
