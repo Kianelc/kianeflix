e9aaa406201cc319481d06d8f1b09482b5a6dc6d
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2020-07-29 21:52:57 -0300

    Tornando a página SPA

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Logo from "../../assets/img/logo.png";
 import "./Menu.css";
-//import ButtonLink from "./components/ButtonLink";
+import { Link } from "react-router-dom";
 import Button from "../Button";
 
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="Kianeflix logo" />
-      </a>
-      <Button as="a" className="ButtonLink" href="/">
+      </Link>
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo vídeo
       </Button>
     </nav>
