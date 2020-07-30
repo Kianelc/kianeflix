f172818832e4ce43c2453a187a258dce5b224dd4
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2020-07-29 21:52:43 -0300

    Padronização da nomenclatura e separação de dependencias

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Home from "./pages/Home";
 
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
-function CadastroVideo() {
-  return <div>Página de cadastro de vídeo</div>;
-}
+import CadastroVideo from "./pages/cadastro/Video";
 
 const Pagina404 = () => <div>Página 404</div>;
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={App} exact />
+      <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
       <Route component={Pagina404} />
     </Switch>
