97240a48d963027e52b9ae62f7ce18a9f87a09b7
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2020-07-29 21:18:15 -0300

    começando a incluir router

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+function CadastroVideo() {
+  return <div>Página de cadastro de vídeo</div>;
+}
+
+const Pagina404 = () => <div>Página 404</div>;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
