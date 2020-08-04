4349ba6378e6bfcbbfac858e85a4a26bf2e5dd53
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2020-08-04 02:36:20 -0300

    adicionando uma nova função no request de categorias

--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -2,6 +2,16 @@ import config from "../config";
 
 const URL_CATEGORIES = `${config.URL_BACKEND}/categorias`;
 
+function getAll() {
+  return fetch(`${URL_CATEGORIES}`).then(async (respostaDoServidor) => {
+    if (respostaDoServidor.ok) {
+      const resposta = await respostaDoServidor.json();
+      return resposta;
+    }
+    throw new Error("Não foi possível pegar os dados!");
+  });
+}
+
 function getAllWithVideos() {
   return fetch(`${URL_CATEGORIES}?_embed=videos`).then(async (respostaDoServidor) => {
     if (respostaDoServidor.ok) {
@@ -13,5 +23,6 @@ function getAllWithVideos() {
 }
 
 export default {
-  getAllWithVideos
+  getAllWithVideos,
+  getAll
 };
