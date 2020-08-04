18d77f1dfaaf83b7cea7ca2a82f12bcffe67279b
Kiane Lucia Casagrande <kiane.casagrande@hotmail.com>
2020-08-04 02:35:54 -0300

    mostrando a lista de categorias já cadastradas no servidor

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import PageDefault from "../../../components/PageDefault";
 import useForm from "../../../hooks/useForm";
 import FormField from "../../../components/FormField";
 import Button from "../../../components/Button";
 import videoRepository from "../../../repositories/videos";
+import categoriasRepository from "../../../repositories/categorias";
 
 function CadastroVideo() {
   const history = useHistory();
@@ -13,16 +14,26 @@ function CadastroVideo() {
     url: "https://www.youtube.com/watch?v=jOAU81jdi-c",
     categoria: "Front End"
   };
+  const [categorias, setCategorias] = useState([]);
+  const categoriaTitulo = categorias.map(({ titulo }) => titulo);
   const { handleChange, valores } = useForm(valoresIniciais);
+  useEffect(() => {
+    categoriasRepository.getAll().then((categoriasDoServidor) => {
+      setCategorias(categoriasDoServidor);
+    });
+  }, []);
   return (
     <PageDefault>
       <h1>Cadastro de Vídeo</h1>
       <form onSubmit={(event) => {
         event.preventDefault();
+        const categoriaEscolhida = categorias.find((categoria) => {
+          return categoria.titulo === valores.categoria;
+        });
         videoRepository.create({
           titulo: valores.titulo,
           url: valores.url,
-          categoriaId: 1
+          categoriaId: categoriaEscolhida
         }).then(() => {
           history.push("/");
         });
@@ -48,6 +59,7 @@ function CadastroVideo() {
           type="text"
           value={valores.categoria}
           onChange={handleChange}
+          suggestions={categoriaTitulo}
         />
         <Button type="submit">
           Cadastrar
